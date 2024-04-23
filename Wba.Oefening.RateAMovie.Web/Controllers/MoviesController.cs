607cbd970f1e13f8756e3bfb9e057c78014f15af
Mileto Di Marco <mileto.di.marco@howest.be>
2024-04-23 16:16:49 +0200

    Add update image

--- Wba.Oefening.RateAMovie.Web/Controllers/MoviesController.cs
+++ Wba.Oefening.RateAMovie.Web/Controllers/MoviesController.cs
@@ -304,7 +304,134 @@ namespace Wba.Oefening.RateAMovie.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Update(MoviesUpdateViewModel moviesUpdateViewModel)
         {
+            //custom validation
+            if (moviesUpdateViewModel.ReleaseDate > DateTime.Now)
+            {
+                ModelState.AddModelError("ReleaseDate", "Date must be in the past!");
+            }
+            //model validation
+            if (!ModelState.IsValid)
+            {
+                //return model to the view
+                moviesUpdateViewModel.Actors = await _movieContext
+                            .Actors.Select
+                            (a => new PersonCheckbox
+                            {
+                                Id = a.Id,
+                                Name = $"{a.FirstName} {a.LastName}"
+                            }).ToListAsync();
+                moviesUpdateViewModel.Directors = await _movieContext
+                            .Directors.Select(d => new SelectListItem
+                            {
+                                Value = d.Id.ToString(),
+                                Text = $"{d.FirstName} {d.LastName}"
+                            }).ToListAsync();
+                moviesUpdateViewModel.Companies = await _movieContext
+                            .Companies.Select(c => new SelectListItem
+                            {
+                                Value = c.Id.ToString(),
+                                Text = $"{c.Name}"
+                            }).ToListAsync();
+                return View(moviesUpdateViewModel);
+            }
+            //get the movie
+            var movie = await _movieContext.Movies
+                .Include(m => m.Company)
+                .Include(m => m.Actors)
+                .Include(m => m.Directors)
+                .FirstOrDefaultAsync
+                (m => m.Id == moviesUpdateViewModel.Id);
+            if(movie == null)
+            {
+                Response.StatusCode = 404;
+                return View("Error", new ErrorViewModel { ErrorMessage = "Movie not found!"});
+            }
+            var selectedActorIds = moviesUpdateViewModel
+                .Actors.Where(a => a.Selected == true).Select(a => a.Id);
+            movie.Title = moviesUpdateViewModel.Title;
+            movie.ReleaseDate = moviesUpdateViewModel.ReleaseDate;
+            movie.CompanyId = moviesUpdateViewModel.SelectedCompanyId;
+            movie.Directors.Clear();
+            movie.Directors = await _movieContext
+                .Directors.Where(d => moviesUpdateViewModel
+                .SelectedDirectorIds.Contains(d.Id)).ToListAsync();
+            movie.Actors.Clear();
+            movie.Actors = await _movieContext
+                    .Actors.Where(d => selectedActorIds.Contains(d.Id)).ToListAsync();
             
+            //check for image
+            if (moviesUpdateViewModel.Image != null)
+            {
+                //handle image
+                //build path to file
+                var fileName = movie.ImageFileName;
+                //build path to store file
+                var pathToImageFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "movies");
+                //check if path exists
+                if (!Directory.Exists(pathToImageFolder))
+                {
+                    //create directory
+                    try
+                    {
+                        Directory.CreateDirectory(pathToImageFolder);
+                    }
+                    catch (DirectoryNotFoundException directoryNotFoundException)
+                    {
+                        _logger.LogCritical(directoryNotFoundException.Message);
+                        ModelState.AddModelError("", "Something went wrong, please try again later");
+                    }
+                }
+                //copy file to full path
+                var fullPathToFile = Path.Combine(pathToImageFolder, fileName);
+                using (FileStream fileStream = new FileStream(fullPathToFile, FileMode.Create))
+                {
+                    try
+                    {
+                        await moviesUpdateViewModel.Image.CopyToAsync(fileStream);
+                    }
+                    catch (FileNotFoundException fileNotFoundException)
+                    {
+                        _logger.LogCritical(fileNotFoundException.Message);
+                        ModelState.AddModelError("", "Something went wrong, please try again later");
+                    }
+                }
+                if (!ModelState.IsValid)
+                {
+                    moviesUpdateViewModel.Actors = await _movieContext
+                            .Actors.Select
+                            (a => new PersonCheckbox
+                            {
+                                Id = a.Id,
+                                Name = $"{a.FirstName} {a.LastName}"
+                            }).ToListAsync();
+                    moviesUpdateViewModel.Directors = await _movieContext
+                                .Directors.Select(d => new SelectListItem
+                                {
+                                    Value = d.Id.ToString(),
+                                    Text = $"{d.FirstName} {d.LastName}"
+                                }).ToListAsync();
+                    moviesUpdateViewModel.Companies = await _movieContext
+                                .Companies.Select(c => new SelectListItem
+                                {
+                                    Value = c.Id.ToString(),
+                                    Text = $"{c.Name}"
+                                }).ToListAsync();
+                    return View(moviesUpdateViewModel);
+                }
+                //add filename to movie entity
+                movie.ImageFileName = fileName;
+            }
+            //save the changes
+            try
+            {
+                await _movieContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                _logger.LogError(dbUpdateException.Message);
+                return View("Error", new ErrorViewModel { ErrorMessage = "Movie not created!" });
+            }
+            return RedirectToAction("Index");
         }
     }
 }
